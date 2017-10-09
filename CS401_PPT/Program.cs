ab67aa05535e07310875310931245291521adb47
xiang wei <wei19971@hotmail.com>
2017-10-09 10:31:25 +0800

    hh

--- CS401_PPT/Program.cs
+++ CS401_PPT/Program.cs
@@ -11,6 +11,7 @@ namespace CS401_PPT
         static void Main(string[] args)
         {
             Console.WriteLine("Hello");
+            Console.WriteLine("bb");
             Console.WriteLine("Bye");
         }
     }
